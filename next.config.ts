42e512252d464365c05cf63d9f68667304d723bd
Athaya <risyad.athaya2007@gmail.com>
2026-08-11 12:41:30 +0700

    initial commit

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,10 @@
 import type { NextConfig } from "next";
 
+// next.config.js
+module.exports = {
+    allowedDevOrigins: ['192.168.0.102'],
+}
+
 const nextConfig: NextConfig = {
   /* config options here */
 };
