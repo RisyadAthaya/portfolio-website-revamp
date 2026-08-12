1fbe04b9f519182f98f6c5f2a54c76e620ee6968
Athaya <risyad.athaya2007@gmail.com>
2026-08-12 08:25:02 +0700

    feat(font): changed the font to bodoni moda

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,29 +1,26 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Bodoni_Moda } from 'next/font/google'
 import "../styles/globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Risyad Athaya",
   description: "Portfolio website of Risyad Athaya",
 };
 
+export const bodoniModa = Bodoni_Moda({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  style: ['normal', 'italic'],
+  display: 'swap',
+});
+
 export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased scrollbar-hide`}
+      className={`h-full antialiased scrollbar-hide`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className={`${bodoniModa.className} min-h-full flex flex-col`}>{children}</body>
     </html>
   );
 }
