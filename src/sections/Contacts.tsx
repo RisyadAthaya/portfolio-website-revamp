6992157c316888f9839431de80729a320f249f05
Athaya <risyad.athaya2007@gmail.com>
2026-08-14 23:46:31 +0700

    feat(footer): add a basic footer

--- src/sections/Contacts.tsx
+++ src/sections/Contacts.tsx
@@ -1,3 +1,18 @@
 export default function Contacts() {
-  return <></>
+  return (
+    <div className="flex flex-row flex-wrap items-center justify-center gap-4 md:justify-between w-full px-16 py-8">
+      <p className="text-size-200">Let&apos;s work together!</p>
+      <ul className="flex flex-row flex-wrap items-center justify-between gap-8">
+        <li>
+          Email
+        </li>
+        <li>
+          Linkedin
+        </li>
+        <li>
+          Github
+        </li>
+      </ul>
+    </div>
+  )
 }
