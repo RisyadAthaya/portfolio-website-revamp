d079b014c39bbdb9cd92867ce032d7cca85fde7d
Athaya <risyad.athaya2007@gmail.com>
2026-08-15 10:48:59 +0700

    fix(hero): apply animation to the video

--- src/components/hero/AnimatedHero.tsx
+++ src/components/hero/AnimatedHero.tsx
@@ -6,29 +6,44 @@ import gsap from 'gsap'
 
 gsap.registerPlugin(useGSAP)
 
-export default function AnimatedHeroText() {
+export default function AnimatedHero() {
   const containerRef = useRef<HTMLDivElement>(null)
   const titleRef = useRef<HTMLHeadingElement>(null)
   const subtitleRef = useRef<HTMLParagraphElement>(null)
+  const videoRef = useRef<HTMLVideoElement>(null)
   const subtitleClasses = 'text-subtitle text-size-300 md:text-size-500 top-0'
   const subtitleTexts = ['CSUI Student', 'AI Engineer', 'Software Engineer']
 
   useGSAP(() => {
     const titleElement = titleRef.current
     const subtitleElement = subtitleRef.current
+    const videoElement = videoRef.current
 
     if (!titleElement || !subtitleElement) return
 
     const timeline = gsap.timeline()
     timeline.set(titleElement, { opacity: 0, yPercent: 60 })
     timeline.set(subtitleElement, { opacity: 0, xPercent: -10 })
+    timeline.set(videoElement, { opacity: 0 })
 
+    // Disable scrolling for a while
+    timeline.call(() => {
+      document.body.style.overflow = 'hidden'
+    })
     timeline.to(titleElement, {
       opacity: 1,
       yPercent: 0,
       duration: 1.5,
       ease: 'power1.out',
     })
+      .to(videoElement, {
+        opacity: 1,
+        duration: 0.8,
+      })
+      // Enable scrolling again
+      .call(() => {
+        document.body.style.overflow = ''
+      })
 
     const subtitlesTimeline = gsap.timeline({ repeat: -1 })
 
@@ -46,15 +61,29 @@ export default function AnimatedHeroText() {
   }, { scope: containerRef })
 
   return (
-    <div ref={containerRef} className="flex flex-col z-10 items-center justify-center">
-      <h1 ref={titleRef} className="text-title text-size-600 md:text-size-900 leading-none">
-        ATHAYA
-      </h1>
-      <span className="flex justify-center">
+    <>
+      <div ref={containerRef} className="flex flex-col z-10 items-center justify-center">
+        <h1 ref={titleRef} className="text-title text-size-600 md:text-size-900 leading-none text-shadow-[0_0_8px_#ffffff]">
+          ATHAYA
+        </h1>
+        <span className="flex justify-center">
         <p className={subtitleClasses} ref={subtitleRef}>
           CSUI Student
         </p>
       </span>
-    </div>
+      </div>
+      <div aria-hidden="true" className="flex flex-col items-center absolute inset-0 z-0 pointer-events-none">
+        <video
+          loop
+          autoPlay
+          muted
+          playsInline
+          preload="auto"
+          src="/videos/synthwaveBackground.mp4"
+          ref={videoRef}
+          className="min-h-dvh w-full object-cover"
+        />
+      </div>
+    </>
   )
 }
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,20 +1,9 @@
-import AnimatedHeroText from "@/src/components/hero/AnimatedHeroText";
+import AnimatedHero from "@/src/components/hero/AnimatedHero";
 
 export default function Hero() {
   return (
     <section className="flex flex-col justify-center items-center min-h-dvh w-full overflow-hidden">
-      <AnimatedHeroText />
-      <div aria-hidden="true" className="flex flex-col items-center absolute inset-0 z-0 pointer-events-none">
-        <video
-          loop
-          autoPlay
-          muted
-          playsInline
-          preload="auto"
-          src="/videos/synthwaveBackground.mp4"
-          className="min-h-dvh w-full object-cover"
-        />
-      </div>
+      <AnimatedHero />
     </section>
   )
 }
