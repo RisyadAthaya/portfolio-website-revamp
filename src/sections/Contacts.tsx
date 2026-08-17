0bb445839486bf50c5d1de436071ff6d1927a103
Athaya <risyad.athaya2007@gmail.com>
2026-08-17 11:56:41 +0700

    feat(contacts): add anchor to each footer's links

--- src/sections/Contacts.tsx
+++ src/sections/Contacts.tsx
@@ -1,17 +1,30 @@
+const LINKS = [
+  {
+    text: 'Email',
+    link: 'mailto:risyad.athaya@ristek.cs.ui.ac.id'
+  },
+  {
+    text: 'LinkedIn',
+    link: 'https://www.linkedin.com/in/risyadathaya/'
+  },
+  {
+    text: 'Github',
+    link: 'https://github.com/RisyadAthaya'
+  },
+]
+
 export default function Contacts() {
   return (
     <div className="flex flex-row flex-wrap items-center justify-center gap-4 md:justify-between w-full px-16 py-8">
       <p className="text-size-200">Let&apos;s work together!</p>
       <ul className="flex flex-row flex-wrap items-center justify-between gap-8">
-        <li>
-          Email
-        </li>
-        <li>
-          Linkedin
-        </li>
-        <li>
-          Github
-        </li>
+        {LINKS.map((link) => (
+          <li key={link.text}>
+            <a href={link.link} target="_blank">
+              <p className="text-white hover:text-gray-400">{link.text}</p>
+            </a>
+          </li>
+        ))}
       </ul>
     </div>
   )
