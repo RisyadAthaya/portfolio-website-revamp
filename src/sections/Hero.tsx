f876635e8b2312cec93e7fc87480c24debddc8f1
Athaya <risyad.athaya2007@gmail.com>
2026-08-17 13:29:07 +0700

    fix(hero): hero section constantly readjusting in mobile

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -2,7 +2,7 @@ import AnimatedHero from '@/src/components/hero/AnimatedHero'
 
 export default function Hero() {
   return (
-    <section className="flex flex-col justify-center items-center min-h-dvh w-full overflow-hidden">
+    <section className="flex flex-col justify-center items-center h-svh w-full overflow-hidden">
       <AnimatedHero />
     </section>
   )
