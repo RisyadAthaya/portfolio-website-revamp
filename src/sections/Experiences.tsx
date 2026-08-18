f65cc1e6a5951e879c1e4c8e03f85a087c8d4075
Athaya <risyad.athaya2007@gmail.com>
2026-08-18 08:54:38 +0700

    Revert "feat: add experiences section and add new text size"
    This reverts commit 6dee18e11a705cc1eb114be45528b79ff62dd3a4.

--- src/sections/Experiences.tsx
+++ src/sections/Experiences.tsx
@@ -1,35 +1,3 @@
-const experiences = [
-  {
-    orgName: 'RISTEK',
-    position: 'People Operations of Data Science & AI',
-    date: 'Mar 2026 - Present',
-  },
-  {
-    orgName: 'COMPFEST',
-    position: 'Data Science Academy Expert Staff',
-    date: 'Jan 2026 - Present',
-  },
-]
-
 export default function Experiences() {
-  return (
-    <div className="flex flex-col items-center gap-8 py-16 w-full">
-      <h2 className="text-size-500 md:text-size-700">Experiences</h2>
-      <ul className="flex flex-col items-center md:items-start px-16 w-full">
-        {experiences.map((experience) => (
-          <li
-            key={experience.orgName}
-            className="flex flex-col md:flex-row justify-between gap-2 md:gap-8 items-center
-            w-full border-b-white border-b-2 last:border-0 py-6 md:py-8 text-size-500 md:text-size-600"
-          >
-            <p>{experience.orgName}</p>
-            <div className="flex flex-col text-center md:text-right text-size-100 md:text-size-200">
-              <p>{experience.position}</p>
-              <p>{experience.date}</p>
-            </div>
-          </li>
-        ))}
-      </ul>
-    </div>
-  )
+  return <></>
 }
--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -3,7 +3,7 @@ import ProjectsCarousel from '@/src/components/projects/ProjectsCarousel'
 export default function Projects() {
   return (
     <section className="flex flex-col items-center w-full mb-2">
-      <h2 className="text-size-500 md:text-size-700">Projects</h2>
+      <h2 className="text-size-500 md:text-size-600">Projects</h2>
       <ProjectsCarousel />
     </section>
   )
